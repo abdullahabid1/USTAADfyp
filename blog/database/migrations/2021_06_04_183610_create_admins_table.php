45bc53c142b9a6393d9c118eb0958c4b2d3870e3
abdullahabid1 <84861256+abdullahabid1@users.noreply.github.com>
2021-06-05 00:19:41 +0500

    AB migrate table

--- blog/database/migrations/2021_06_04_183610_create_admins_table.php
+++ blog/database/migrations/2021_06_04_183610_create_admins_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTeachersTable extends Migration
+class CreateAdminsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,17 +13,9 @@ class CreateTeachersTable extends Migration
      */
     public function up()
     {
-        Schema::create('teachers', function (Blueprint $table) {
+        Schema::create('admins', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
-        $table->string('firstName');
-        $table->string('lastName');
-        $table->string('email');
-        $table->integer('phone');
-        $table->string('password');
-        $table->string('gender');    
-      
-      
         });
     }
 
@@ -34,6 +26,6 @@ class CreateTeachersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('teachers');
+        Schema::dropIfExists('admins');
     }
 }
